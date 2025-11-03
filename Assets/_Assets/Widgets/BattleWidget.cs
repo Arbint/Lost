e2a4962941fb8dc01e3edf9e1e8d4239a189e968
Li <jili1@uiwtx.edu>
2025-11-03 15:21:48 -0600

    adding ability navigation

--- Assets/_Assets/Widgets/BattleWidget.cs
+++ Assets/_Assets/Widgets/BattleWidget.cs
@@ -1,6 +1,7 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
 
 public class BattleWidget : MonoBehaviour
 {
@@ -8,29 +9,35 @@ public class BattleWidget : MonoBehaviour
     [SerializeField] LayoutGroup mAbilityListLayoutGroup;
     [SerializeField] AbilityWidget mAbilityWidgetPrefab;
 
+    List<AbilityWidget> mAbilityWidgets = new List<AbilityWidget>();
+
     public void SetCharacterControlTarget(BattleCharacter battleCharacter)
     {
         foreach(Transform existingEntries in mAbilityListLayoutGroup.transform)
         {
-            //Destroy(existingEntries.gameObject);
-            Debug.Log($"this is a new log");
+            Destroy(existingEntries.gameObject);
         }
+
+        mAbilityWidgets.Clear();
 
         mCharacterControlWidget.gameObject.SetActive(true);
         mCharacterControlWidget.SetBattleCharacter(battleCharacter);
         AbilityComponent abilityComponent = battleCharacter.GetAbilityComponent();
-        if(abilityComponent)
+        if (abilityComponent)
         {
-            foreach(Ability ability in abilityComponent.GetAbilities())
+            foreach (Ability ability in abilityComponent.GetAbilities())
             {
                 AddAbilityToAbilityList(ability);
             }
         }
+
+        EventSystem.current.SetSelectedGameObject(mAbilityWidgets[0].gameObject); 
     }
 
     private void AddAbilityToAbilityList(Ability ability)
     {
         AbilityWidget newAbilityWidget = Instantiate(mAbilityWidgetPrefab, mAbilityListLayoutGroup.transform);
+        mAbilityWidgets.Add(newAbilityWidget);
         newAbilityWidget.SetAbility(ability);
     }
 }
